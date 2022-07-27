c7b01b91a191560b3dfd428e9a6a8d3a064e1b92
InTae <kit938639@gmail.com>
2022-07-27 15:02:01 +0900

    Testing PerformanceReview Entities and repositories

--- Troupe/backend/src/test/java/com/troupe/backend/repository/performance/PerformanceReviewTest.java
+++ Troupe/backend/src/test/java/com/troupe/backend/repository/performance/PerformanceReviewTest.java
@@ -2,7 +2,7 @@ package com.troupe.backend.repository.performance;
 
 import com.troupe.backend.domain.member.Member;
 import com.troupe.backend.domain.performance.Performance;
-import com.troupe.backend.domain.performance.PerformanceSave;
+import com.troupe.backend.domain.performance.PerformanceReview;
 import com.troupe.backend.repository.member.MemberRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,7 +18,8 @@ import java.text.SimpleDateFormat;
 @DataJpaTest
 @Transactional(propagation = Propagation.NOT_SUPPORTED)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class PerformanceSaveTest {
+public class PerformanceReviewTest {
+
     @Autowired
     MemberRepository memberRepository;
 
@@ -26,22 +27,28 @@ public class PerformanceSaveTest {
     PerformanceRepository performanceRepository;
 
     @Autowired
-    PerformanceSaveRepository performanceSaveRepository;
+    PerformanceReviewRepository performanceReviewRepository;
 
     @Test
     public void saveTest() throws ParseException  {
         Member member = memberRepository.getById(3);
-        Performance performance = performanceRepository.getById(2);
+        Performance performance = performanceRepository.getById(1);
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 
-        PerformanceSave performanceSave = PerformanceSave.builder()
+        PerformanceReview performanceReview = PerformanceReview.builder()
                 .memberNo(member)
                 .pfNo(performance)
                 .createdTime(dateFormat.parse("20220802"))
+                .isModified(false)
+                .isRemoved(false)
+                .content("댓글 1")
                 .build();
 
-        PerformanceSave savePerformanceSave = performanceSaveRepository.save(performanceSave);
+        PerformanceReview savePerformanceReview = performanceReviewRepository.save(performanceReview);
+
+        Assertions.assertEquals("댓글 1", savePerformanceReview.getContent());
 
     }
+
 }
