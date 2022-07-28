f072d010f76555ed068f579de7c7e32dd9ffa970
Doyoung Yim <dyim0403@gmail.com>
2022-07-28 13:08:17 +0900

    guestbook autowire

--- Troupe/backend/src/main/java/com/troupe/backend/service/member/GuestbookService.java
+++ Troupe/backend/src/main/java/com/troupe/backend/service/member/GuestbookService.java
@@ -1,8 +1,23 @@
 package com.troupe.backend.service.member;
 
+import com.troupe.backend.repository.member.GuestbookRepository;
+import com.troupe.backend.repository.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GuestbookService {
+    private GuestbookRepository guestbookRepository;
 
+    private MemberRepository memberRepository;
+
+    @Autowired
+    public void setGuestbookRepository(GuestbookRepository guestbookRepository) {
+        this.guestbookRepository = guestbookRepository;
+    }
+
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 }
