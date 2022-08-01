3a93cef7fe1b74214967c4b0bdb08fd5aef9ed13
intae <kit938639@gmail.com>
2022-08-01 18:00:45 +0900

    feat : 공연 후기 삭제, 공연 후기 목록

--- Troupe/backend/src/main/java/com/troupe/backend/service/Performance/PerformanceReviewService.java
+++ Troupe/backend/src/main/java/com/troupe/backend/service/Performance/PerformanceReviewService.java
@@ -3,6 +3,7 @@ package com.troupe.backend.service.Performance;
 import com.troupe.backend.domain.member.Member;
 import com.troupe.backend.domain.performance.Performance;
 import com.troupe.backend.domain.performance.PerformanceReview;
+import com.troupe.backend.dto.Performance.PfReviewResponse;
 import com.troupe.backend.repository.member.MemberRepository;
 import com.troupe.backend.repository.performance.PerformanceRepository;
 import com.troupe.backend.repository.performance.PerformanceReviewRepository;
@@ -15,10 +16,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -61,6 +59,48 @@ public class PerformanceReviewService {
                 .build();
 
         performanceReviewRepository.save(performanceReview);
+    }
+
+    /**
+     * 공연 후기 삭제
+     * @param pfNo
+     * @param reviewNo
+     */
+    @Transactional
+    public void delete(int pfNo, int reviewNo) {
+        Performance performance = performanceRepository.findById(pfNo)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 공연입니다."));
+        //  refactoring : 리뷰 번호로만 삭제해도 되지않을까?
+        PerformanceReview performanceReview = performanceReviewRepository.findBypfNoAndId(performance, reviewNo)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 댓글입니다."));
+        performanceReview.setRemoved(true);
+        performanceReviewRepository.save(performanceReview);
+    }
+
+    /**
+     * 공연 후기 리스트 반환
+     * @param pfNo
+     * @param memberNo
+     * @return
+     */
+    @Transactional(readOnly = true)
+    public List<PfReviewResponse> findPfReviewList(int pfNo, int memberNo){
+        Member member = memberRepository.findById(memberNo)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 유저입니다."));
+        Performance performance = performanceRepository.findById(pfNo)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 공연입니다."));
+        List<PerformanceReview> performanceReviewList = performanceReviewRepository.findByPfNo(performance);
+        List<PfReviewResponse> pfReviewResponseList = new ArrayList<>();
+        for (PerformanceReview performanceReview : performanceReviewList){
+            pfReviewResponseList.add( PfReviewResponse.builder()
+                    .memberNo(memberNo)
+                    .nickname(member.getNickname())
+                    .profileImageUrl(member.getProfileImageUrl())
+                    .comment(performanceReview.getContent())
+                    .build()
+            );
+        }
+        return pfReviewResponseList;
     }
 
 
