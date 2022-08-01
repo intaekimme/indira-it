9e16fa446709afb831a50df26bdf26c0189bca32
Doyoung Yim <dyim0403@gmail.com>
2022-08-01 13:11:47 +0900

    profile controller - follow

--- Troupe/backend/src/main/java/com/troupe/backend/util/MyConstant.java
+++ Troupe/backend/src/main/java/com/troupe/backend/util/MyConstant.java
@@ -3,9 +3,9 @@ package com.troupe.backend.util;
 public final class MyConstant {
     public static final String SUCCESS = "success";
     public static final String FAIL = "fail";
-
     public static final String MESSAGE = "message";
 
+    // 유저 관련
     public static final String MEMBER_NO = "memberNo";
     public static final String EMAIL = "email";
     public static final String NICKNAME = "nickname";
@@ -19,6 +19,22 @@ public final class MyConstant {
     public static final String PROFILE_IMAGE_URL = "profileImageUrl";
     public static final String ACCESS_TOKEN = "accessToken";
     public static final String CONTENT = "content";
+
+    // 팔로우 관련
+    public static final String FAN_LIST = "fanList";
+    public static final String FAN_COUNT = "fanCount";
+    public static final String STAR_LIST = "starList";
+    public static final String IS_FOLLOWING = "isFollowing";
+
+    // 태그 관련
+    public static final String TAG_LIST = "tagList";
+
+    // 호감도 관련
+    public static final String EXP = "exp";
+    public static final String LEVEL = "level";
+    public static final String EXP_TO_NEXT_LEVEL = "expToNextLevel";
+
+    // 파일 서버 주소
     public static final String FILE_SERVER_URL = "https://s3.ap-northeast-2.amazonaws.com/hongjoo.troupe.project/";
 
 }
