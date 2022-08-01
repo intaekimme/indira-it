2db7e8398f8a202bed5951989690cd1952a07b1b
intae <kit938639@gmail.com>
2022-08-01 16:30:18 +0900

    feat : 프로필 주인의 공연 북마크 목록, 프로필 주인이 등록한 공연 목록, 공연 후기 등록

--- Troupe/backend/src/main/java/com/troupe/backend/repository/performance/PerformanceSaveRepository.java
+++ Troupe/backend/src/main/java/com/troupe/backend/repository/performance/PerformanceSaveRepository.java
@@ -4,6 +4,7 @@ import com.troupe.backend.domain.member.Member;
 import com.troupe.backend.domain.performance.Performance;
 import com.troupe.backend.domain.performance.PerformanceSave;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,22 +25,33 @@ public interface PerformanceSaveRepository extends JpaRepository<PerformanceSave
      * @param performance
      * @return
      */
-//    PerformanceSave findByMemberNoAndPfNoAndRemovedTrue(Member member, Performance performance);
-//
-//    /**
-//     * 유저 번호와 공연 번호로 유저가 저장한 특정 공연 1개 반환
-//     * @param member
-//     * @param performance
-//     * @return
-//     */
-//    PerformanceSave findByMemberNoAndPfNoAndRemovedFalse(Member member, Performance performance);
-//
-//    /**
-//     * 유저가 저장한 공연 목록 반환
-//     * @param member
-//     * @return
-//     */
-//    List<PerformanceSave> findByMemberNoAndRemovedFalse(Member member);
+    @Query("select pfs from PerformanceSave pfs " +
+            "where pfs.memberNo = :member " +
+            "and pfs.pfNo = :performance " +
+            "and pfs.isRemoved = true ")
+    PerformanceSave findByMemberNoAndPfNoAndRemovedTrue(Member member, Performance performance);
+
+    /**
+     * 유저 번호와 공연 번호로 유저가 저장한 특정 공연 1개 반환
+     * @param member
+     * @param performance
+     * @return
+     */
+    @Query("select pfs from PerformanceSave pfs " +
+            "where pfs.memberNo = :member " +
+            "and pfs.pfNo = :performance " +
+            "and pfs.isRemoved = false ")
+    PerformanceSave findByMemberNoAndPfNoAndRemovedFalse(Member member, Performance performance);
+
+    /**
+     * 유저가 저장한 공연 목록 반환
+     * @param member
+     * @return
+     */
+    @Query("select pfs from PerformanceSave pfs " +
+            "where pfs.memberNo = :member " +
+            "and pfs.isRemoved = false ")
+    List<PerformanceSave> findByMemberNoAndRemovedFalse(Member member);
 
     /**
      * 유저 번호와 공연 번호로 찾은 공연 삭제
