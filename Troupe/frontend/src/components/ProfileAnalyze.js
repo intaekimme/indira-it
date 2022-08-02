e8e8da0b20a3a22138694e10299e7cb03388fb3a
박민호 <tkdfhralsgh@naver.com>
2022-08-02 09:34:34 +0900

    유저페이지 호감도 진행중

--- Troupe/frontend/src/components/ProfileAnalyze.js
+++ Troupe/frontend/src/components/ProfileAnalyze.js
@@ -32,17 +32,28 @@ export default function ProfileAnalyze(props) {
   const [followThisUser, setFollowThisUser] = React.useState(true);
   //나와의 호감도보기
   const [likeabilityWithUser, setLikeabilityWithUser] = React.useState(false);
-  // follow/unfollow 버튼클릭
-  const followClick = () => {
-    const currnetFollow = followThisUser;
-    setFollowThisUser((current) => !current);
-    const data = {
-      follow: !currnetFollow,
-      userNo: props.userNo,
-      currentUser: 1,
-    };
-    apiClient.follow(data);
-  };
+  //개인분석 결과
+  const [interest, setInterest] = React.useState([10,20,30,40,50,60,70,80]);
+  //호감도 순위
+  // const [likeabilityData, setLikeabilityData] = React.useState([apiClient.getLikeabilityData]);
+  const [likeabilityData, setLikeabilityData] = React.useState([
+    {
+      starUserNo: 1,
+      nickname: "첫번째",
+      exp: 1520
+    },
+    {
+      starUserNo: 2,
+      nickname: "두번째",
+      exp: 1250
+    },
+    {
+      starUserNo: 1,
+      nickname: "세번째",
+      exp: 970
+    },
+  ]);
+  // setInterest(apiClient.interest(props.userNo));
   // 나와의 호감도/ 이유저의 호감도 보기 버튼클릭
   const changeLikeability = () => {
     setLikeabilityWithUser((current) => !current);
@@ -57,7 +68,7 @@ export default function ProfileAnalyze(props) {
   return (
     <Card
       sx={{ width: props.width }}
-      style={{ border: "3px solid black", backgroundColor: "#FFCF24" }}
+      style={{ border: "5px solid #FFCF24", backgroundColor: "#FFFFFF" }}
     >
       <CardContent>
         {likeabilityWithUser ? (
@@ -82,7 +93,11 @@ export default function ProfileAnalyze(props) {
               </div>
             </Grid>
             <Grid item xs={12}>
-              <LikeabilityRank nickname={props.nickname} likeabilityWithUser={likeabilityWithUser}></LikeabilityRank>
+              <LikeabilityRank
+              nickname={props.nickname}
+              likeabilityWithUser={likeabilityWithUser}
+              likeabilityData = {likeabilityData}
+              ></LikeabilityRank>
             </Grid>
           </Grid>
         ) : (
@@ -118,10 +133,16 @@ export default function ProfileAnalyze(props) {
               )}
             </Grid>
             <Grid item xs={6}>
-              <InterestPolygon nickname={props.nickname}></InterestPolygon>
+              <InterestPolygon nickname={props.nickname} interest={interest}></InterestPolygon>
             </Grid>
             <Grid item xs={6}>
-              <LikeabilityRank nickname={props.nickname} likeabilityWithUser={likeabilityWithUser}></LikeabilityRank>
+              <LikeabilityRank
+                nickname={props.nickname}
+                likeabilityWithUser={likeabilityWithUser}
+                likeabilityData = {likeabilityData}
+                width={props.width}
+                >
+              </LikeabilityRank>
             </Grid>
           </Grid>
         )}
