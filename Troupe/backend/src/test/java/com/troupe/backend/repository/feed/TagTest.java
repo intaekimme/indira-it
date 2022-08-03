ea651495d7c8d2d61f8e4e1f17347c70d9bc651c
hongjoo <dlghdwn10@naver.com>
2022-08-03 11:11:02 +0900

    Closes S07P12A804-131 태그검색 및 피드별 총 좋아요 수 구현 완료

--- Troupe/backend/src/test/java/com/troupe/backend/repository/feed/TagTest.java
+++ Troupe/backend/src/test/java/com/troupe/backend/repository/feed/TagTest.java
@@ -1,21 +1,19 @@
 package com.troupe.backend.repository.feed;
 
 import com.troupe.backend.domain.feed.Feed;
-import com.troupe.backend.domain.feed.FeedImage;
 import com.troupe.backend.domain.feed.FeedTag;
 import com.troupe.backend.domain.feed.Tag;
+import com.troupe.backend.dto.converter.FeedConverter;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Sort;
 
 import javax.transaction.Transactional;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -96,61 +94,74 @@ class TagTest {
         Assertions.assertThat(tags.get(1).getTag().getName()).isEqualTo(saveTag.get(1).getName());
     }
 
+//    @Test
+//    @DisplayName("태그 검색")
+//    public void searchTag(){
+//        // 지금 추가하려는 태그
+//        Optional<Tag> tagInsert1 = tagRepository.findByName("test1");
+//        Optional<Tag> tagInsert2 = tagRepository.findByName("test2");
+//        List<Tag> saveTag = new ArrayList<>();
+//        if(tagInsert1.isEmpty()) {
+//            Tag newTag = tagRepository.save(Tag.builder().name("test1").build());
+//            saveTag.add(newTag);
+//        }
+//        if(tagInsert2.isEmpty()) {
+//            Tag newTag = tagRepository.save(Tag.builder().name("test2").build());
+//            saveTag.add(newTag);
+//        }
+//
+//        //피드 찾고 feedTag 추가
+//        Feed feed = feedRepository.findById(3).get();
+//        Feed feed2 = feedRepository.findById(4).get();
+//        List<FeedTag> feedTags = new ArrayList<>();
+//        feedTags.add(FeedTag.builder().feed(feed).tag(tagRepository.findByName(saveTag.get(0).getName()).get()).build());
+//        feedTags.add(FeedTag.builder().feed(feed).tag(tagRepository.findByName(saveTag.get(1).getName()).get()).build());
+//
+//        feedTags.add(FeedTag.builder().feed(feed2).tag(tagRepository.findByName(saveTag.get(0).getName()).get()).build());
+//        feedTagRepository.saveAll(feedTags);
+//
+//        // 여기부터 실제 로직
+//        // test1 태그 검색
+//        List<FeedTag> feeds = feedTagRepository.findAllByTag(tagRepository.findByName(saveTag.get(0).getName()).get());
+//        // test2 태그 검색
+//        List<FeedTag> feeds2 = feedTagRepository.findAllByTag(tagRepository.findByName(saveTag.get(1).getName()).get());
+//
+//        // test1 test2 둘다 검색
+//        List<FeedTag> feeds3 = feedTagRepository.findAllByTag(saveTag);
+//
+//        // 3번, 4번 피드에 test1 있어서 검색됨
+//        Assertions.assertThat(feeds.size()).isEqualTo(2);
+//        Assertions.assertThat(feeds.get(0).getFeed().getFeedNo()).isEqualTo(3);
+//        Assertions.assertThat(feeds.get(1).getFeed().getFeedNo()).isEqualTo(4);
+//
+//        //4번 피드에 test2 없어서 검색 안됨
+//        Assertions.assertThat(feeds2.size()).isEqualTo(1);
+//        Assertions.assertThat(feeds2.get(0).getFeed().getFeedNo()).isEqualTo(3);
+//
+//        // test1 test2 둘중에 하나라도 있는 피드 검색(일단 보류)
+//        HashSet<Integer> set = new HashSet<>();
+//        set.add(feeds3.get(0).getFeed().getFeedNo());
+//        set.add(feeds3.get(1).getFeed().getFeedNo());
+//        set.add(feeds3.get(2).getFeed().getFeedNo());
+//
+//        Assertions.assertThat(set.size()).isEqualTo(2);
+//        Assertions.assertThat(set.contains(3));
+//        Assertions.assertThat(set.contains(4));
+//        Assertions.assertThat(set.contains(5)).isFalse();
+////        Assertions.assertThat(feeds3.get(0).getFeed().getFeedNo()).isEqualTo(3);
+//    }
+
+    @Autowired
+    FeedTagRepositoryImpl repository;
+
+    @Autowired
+    FeedConverter feedConverter;
     @Test
-    @DisplayName("태그 검색")
-    public void searchTag(){
-        // 지금 추가하려는 태그
-        Optional<Tag> tagInsert1 = tagRepository.findByName("test1");
-        Optional<Tag> tagInsert2 = tagRepository.findByName("test2");
-        List<Tag> saveTag = new ArrayList<>();
-        if(tagInsert1.isEmpty()) {
-            Tag newTag = tagRepository.save(Tag.builder().name("test1").build());
-            saveTag.add(newTag);
-        }
-        if(tagInsert2.isEmpty()) {
-            Tag newTag = tagRepository.save(Tag.builder().name("test2").build());
-            saveTag.add(newTag);
-        }
-
-        //피드 찾고 feedTag 추가
-        Feed feed = feedRepository.findById(3).get();
-        Feed feed2 = feedRepository.findById(4).get();
-        List<FeedTag> feedTags = new ArrayList<>();
-        feedTags.add(FeedTag.builder().feed(feed).tag(tagRepository.findByName(saveTag.get(0).getName()).get()).build());
-        feedTags.add(FeedTag.builder().feed(feed).tag(tagRepository.findByName(saveTag.get(1).getName()).get()).build());
-
-        feedTags.add(FeedTag.builder().feed(feed2).tag(tagRepository.findByName(saveTag.get(0).getName()).get()).build());
-        feedTagRepository.saveAll(feedTags);
-
-        // 여기부터 실제 로직
-        // test1 태그 검색
-        List<FeedTag> feeds = feedTagRepository.findAllByTag(tagRepository.findByName(saveTag.get(0).getName()).get());
-        // test2 태그 검색
-        List<FeedTag> feeds2 = feedTagRepository.findAllByTag(tagRepository.findByName(saveTag.get(1).getName()).get());
-
-        // test1 test2 둘다 검색
-        List<FeedTag> feeds3 = feedTagRepository.findAllByTagIn(saveTag);
-
-        // 3번, 4번 피드에 test1 있어서 검색됨
-        Assertions.assertThat(feeds.size()).isEqualTo(2);
-        Assertions.assertThat(feeds.get(0).getFeed().getFeedNo()).isEqualTo(3);
-        Assertions.assertThat(feeds.get(1).getFeed().getFeedNo()).isEqualTo(4);
-
-        //4번 피드에 test2 없어서 검색 안됨
-        Assertions.assertThat(feeds2.size()).isEqualTo(1);
-        Assertions.assertThat(feeds2.get(0).getFeed().getFeedNo()).isEqualTo(3);
-
-        // test1 test2 둘중에 하나라도 있는 피드 검색(일단 보류)
-        HashSet<Integer> set = new HashSet<>();
-        set.add(feeds3.get(0).getFeed().getFeedNo());
-        set.add(feeds3.get(1).getFeed().getFeedNo());
-        set.add(feeds3.get(2).getFeed().getFeedNo());
-
-        Assertions.assertThat(set.size()).isEqualTo(2);
-        Assertions.assertThat(set.contains(3));
-        Assertions.assertThat(set.contains(4));
-        Assertions.assertThat(set.contains(5)).isFalse();
-//        Assertions.assertThat(feeds3.get(0).getFeed().getFeedNo()).isEqualTo(3);
+    public void test(){
+        List<String> list = new ArrayList<>();
+        list.add("finally");
+        list.add("한");
+        List<FeedTag> feedTags = repository.searchByTags(feedConverter.toTagEntity(list));
+        System.out.println(feedTags);
     }
-
 }
