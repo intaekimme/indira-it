257aabb49879e183e4cd90caf23aaf3f3018079e
hongjoo <dlghdwn10@naver.com>
2022-08-03 15:34:06 +0900

    임시저장

--- Troupe/backend/src/main/java/com/troupe/backend/controller/feed/FeedController.java
+++ Troupe/backend/src/main/java/com/troupe/backend/controller/feed/FeedController.java
@@ -18,7 +18,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.validation.Valid;
@@ -57,11 +56,13 @@ public class FeedController {
         }
     }
 
-    @Operation(summary = "조건별 피드 목록 조회", description = "파라미터: {change} = [all,save,follow] (후에 profileController로 이전-피드저장부분)")
+    @Operation(summary = "조건별 피드 목록 조회(+공연자가 등록한 피드 목록 조회)", description = "파라미터: {change} = [all,save,follow] (후에 profileController로 이전-피드저장부분)")
     @GetMapping("/list/{change}")
-    public ResponseEntity selectAllFeed(@PathVariable String change, Principal principal) throws IOException {
+    public ResponseEntity selectAllFeed(@PathVariable(required = false) String change, Principal principal, @RequestParam(name = "memberNo",required = false) Integer memberNo)  {
         try{
-            List<FeedResponse> feedResponse = feedService.selectAll(change, Integer.parseInt(principal.getName()));
+            List<FeedResponse> feedResponse = null;
+            if(memberNo==null) feedResponse = feedService.selectAll(change, Integer.parseInt(principal.getName()));
+            else feedResponse = feedService.selectAllByMember(memberNo);
             return new ResponseEntity(feedResponse, HttpStatus.OK);
         }catch (Exception e){
             System.out.println(e);
@@ -69,17 +70,6 @@ public class FeedController {
         }
     }
 
-    @Operation(summary = "공연 등록자가 등록한 피드 목록 조회", description = "파라미터: 공연자 멤버 번호(후에 profileController로 이전)")
-    @GetMapping("/{profileMemberNo}/myfeed/list")
-    public ResponseEntity selectAllFeedByPerformer(@PathVariable(name = "profileMemberNo") int memberNo) throws IOException {
-        try{
-            List<FeedResponse> feedResponse = feedService.selectAllByMember(memberNo);
-            return new ResponseEntity(feedResponse, HttpStatus.CREATED);
-        }catch (Exception e){
-            System.out.println(e);
-            return new ResponseEntity("Feed select All FAIL", HttpStatus.BAD_REQUEST);
-        }
-    }
     // responsebody로 수정
     @Operation(summary = "피드 등록", description = "파라미터: 이미지 파일들, 멤버번호, 내용, 태그명들")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -129,7 +119,7 @@ public class FeedController {
         }
     }
 
-    @Operation(summary = "피드 좋아요 스위칭(처음엔 insert, 그 뒤는 update)", description = "파라미터: 피드번호, 멤버번호")
+    @Operation(summary = "피드 좋아요 스위칭", description = "파라미터: 피드번호, 멤버번호 (처음엔 insert, 그 뒤는 update)")
     @PatchMapping("/{feedNo}/like")
     public ResponseEntity likeFeed(@PathVariable int feedNo, Principal principal) throws IOException {
         try {
@@ -141,7 +131,7 @@ public class FeedController {
         }
     }
 
-    @Operation(summary = "피드 저장", description = "파라미터: 피드 번호, 멤버 번호")
+    @Operation(summary = "피드 저장 스위칭", description = "파라미터: 피드 번호, 멤버 번호")
     @PatchMapping("/{feedNo}/save")
     public ResponseEntity saveFeed(@PathVariable int feedNo, Principal principal) throws IOException {
         try {
@@ -257,18 +247,5 @@ public class FeedController {
             return new ResponseEntity("search FAIL", HttpStatus.BAD_REQUEST);
         }
     }
-//    @PatchMapping("/test")
-//    public String  delete(@RequestParam String url) throws  IOException {
-//        service.deleteFile(url);
-//        return "success";
-//    }
-//    @PostMapping("/test")
-//    public String test(@RequestParam MultipartFile file) throws Exception{
-//        try{
-////            service.upload(file,"static");
-//            return  service.upload(file,"static");
-//        }catch (Exception e){
-//            return "fail";
-//        }
-//    }
+
 }
