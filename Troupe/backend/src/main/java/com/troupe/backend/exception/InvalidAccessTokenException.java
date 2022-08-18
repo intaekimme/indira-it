68fc95fcd6ec50ed8528c3058d03d3d624ddb00c
Doyoung Yim <dyim0403@gmail.com>
2022-08-18 10:46:04 +0900

    modify : invalid access token exception

---
+++ Troupe/backend/src/main/java/com/troupe/backend/exception/InvalidAccessTokenException.java
@@ -0,0 +1,8 @@
+package com.troupe.backend.exception;
+
+public class InvalidAccessTokenException extends RuntimeException {
+    public InvalidAccessTokenException() {
+        super("유효하지 않은 액세스 토큰입니다\n");
+    }
+
+}
